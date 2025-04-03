8239717e2ffc5435ba2eac444a53d5c3bafc1d80
Your Name <your.email@example.com>
2025-04-03 07:55:21 +0200

    user py changes

--- models/user.py
+++ models/user.py
@@ -1,13 +1,14 @@
 import uuid
 
 from extensions import db
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = "clients"
     id = db.Column(db.String(255), primary_key=True, default=lambda: str(uuid.uuid4()))
     username = db.Column(db.String(100))
-    password = db.Column(db.String(255))
+    password = db.Column(db.String(400))
 
     # Object -> Dict
     def to_dict(self):
